275fb443ffb5dd1159f1954c2e48cefb25c5a525
Zach Snyder <zachsnyder3@gmail.com>
2018-02-03 21:31:52 -0800

    link OSC server to midiout

--- main_daw.py
+++ main_daw.py
@@ -6,10 +6,12 @@ from pythonosc import dispatcher
 from pythonosc import osc_server
 
 def wifimidi_handler(unused_addr, args, volume):
-    print("unused_addr: {}".format(unused_addr))
-    print("args: {}".format(args))
-    print("volume: {}".format(volume))
-    print("")
+    message = None
+    if volume == 0:
+        message = [0x80, args[0], volume]
+    else:
+        message = [0x90, args[0], volume]
+    args[1].send_message(message)
 
 if __name__ == "__main__":
     midiout = rtmidi.MidiOut()
@@ -22,14 +24,6 @@ if __name__ == "__main__":
         midiout.open_virtual_port("My virtual output")
         print("opening virtual port")
 
-    # plays an octave chromatically
-    #for i in range(12):
-    #note_on = [0x90, 60+i, 112] # channel 1, middle C, velocity 112
-    #note_off = [0x80, 60+i, 0]
-    #midiout.send_message(note_on)
-    #time.sleep(0.5)
-    #midiout.send_message(note_off)
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip",
         default="127.0.0.1", help="The ip to listen on")
@@ -38,7 +32,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/wifimidi", wifimidi_handler)
+    dispatcher.map("/wifimidi", wifimidi_handler, midiout)
 
     server = osc_server.ThreadingOSCUDPServer(
         (args.ip, args.port), dispatcher)
