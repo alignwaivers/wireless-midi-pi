6785142f713e12f6eb01cbea133da01aada50eb2
Zach Snyder <zachsnyder3@gmail.com>
2018-02-03 16:49:42 -0800

    implement Controller class

--- main.py
+++ main.py
@@ -4,26 +4,45 @@ Controller side script, using pygame.midi
 import sys
 import pygame
 import pygame.midi
-        
-
-if __name__ == '__main__':
-    pygame.midi.init()
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+  
+class Controller():
+    """
+    Takes MIDI input from a control surface, sends it over
+    the network to an OSC server running the DAW-side code.
     
-    device_id = int(sys.argv[1]) # device id from first cl argument
-    if device_id is None:
-        input_id = pygame.midi.get_default_input_id()
-    else:
-        input_id =  device_id
-    print("using input_id :{}:".format(input_id))
-    i = pygame.midi.Input(input_id)
+    osc_ip      -->     IP of OSC server on DAW-side
+    osc_port    -->     Port of OSC server on DAW-side
+    device_id   -->     Device ID of MIDI control service attached to pi 
+    """
+    def __init__(self, osc_ip, osc_port, device_id):
+        pygame.midi.init()
+        self.midi_in = pygame.midi.Input(device_id)
+        self.osc_client = udp_client.SimpleUDPClient(osc_ip, osc_port)
+        
+    def run(self):
+        try:
+            while True:
+                if self.midi_in.poll():
+                    midi_events = self.midi_in.read(10)
+                    ############# SEND VIA OSC ###############
+                    print('{}'.format(midi_events[0][0][1:3]))
+                    self.osc_client.send_message("/filter", midi_events[0][0][1:3])
+                    ##########################################
+        except KeyboardInterrupt:
+            pygame.midi.quit()
+      
+if __name__ == '__main__':
+    #osc client setup
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="192.168.0.36",
+        help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=9997,
+        help="The port the OSC server is listening on")
+    parser.add_argument("--deviceid", type=int, default=3,
+        help="The MIDI device id of the control surface")
+    args = parser.parse_args()
 
-    try:
-        while True:
-            if i.poll():
-                midi_events = i.read(10)
-                ############# SEND VIA OSC ###############
-                print('{}'.format(midi_events))
-                ##########################################
-    except KeyboardInterrupt:
-        del i
-        pygame.midi.quit()
+    c = Controller(args.ip, args.port, args.deviceid)
+    c.run()
